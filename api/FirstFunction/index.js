f3b7fd391180cd1839b6ecc964f6abb7f936e251
twinkle13 <twinkle.kheterpal@gmail.com>
2021-06-25 13:49:20 +0530

    api update

--- api/FirstFunction/index.js
+++ api/FirstFunction/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+   // context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
@@ -8,6 +8,6 @@ module.exports = async function (context, req) {
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: responseMessage.toJSON()
     };
 }
